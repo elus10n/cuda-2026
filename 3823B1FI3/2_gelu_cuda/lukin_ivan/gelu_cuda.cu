bd56ab12cb9336b14f46adb064429087f6d196bf
Иван Лукин <dizbvalkyre@gmail.com>
2026-03-13 19:21:09 +0300

    cuda 2

---
+++ 3823B1FI3/2_gelu_cuda/lukin_ivan/gelu_cuda.cu
@@ -0,0 +1,58 @@
+ #include "gelu_cuda.h"
+
+#include <cuda_runtime.h>
+
+__constant__ float PI_coeff = 0.797884F;
+__constant__ float coeff = 0.044715F;
+
+const int block_size = 256;
+
+//__restrict__ - указание nvcc о том, что данные доступны только через этот указатель в данной области видимости. Тем самым мы уменьшаем количество проверок с его стороны
+__global__ void gelu_kernel(const float* __restrict__ input, float* __restrict__ output, const int n)
+{
+    const int id = blockIdx.x*blockDim.x + threadIdx.x; //получаем позицию текущего потока
+    if(id >= n) return; //если вдруг потоков будет больше, чем входных данных
+    
+    float x = input[id];
+    float exp_value = __expf(2.0F * PI_coeff * (x + coeff * x * x * x));
+    float tanh_value = 1.0F - 2.0F / (exp_value + 1.0F);
+    output[id] = 0.5F * x * (1.0F + tanh_value);
+}
+
+std::vector<float> GeluCUDA(const std::vector<float>& input)
+{
+    const int size = input.size();
+    const int num_blocks = (size + block_size - 1) / block_size;
+
+    float *input_gpu;
+    float *output_gpu;
+    const int bytes = size * sizeof(float);
+
+    if(cudaMalloc((void**)&input_gpu, bytes) != cudaSuccess) return input;
+    if(cudaMalloc((void**)&output_gpu, bytes) != cudaSuccess)
+    {
+        cudaFree((void*)input_gpu);
+        return input;
+    }
+    if(cudaMemcpy((void*)input_gpu, (void*)input.data(), bytes, cudaMemcpyHostToDevice) != cudaSuccess)
+    {
+        cudaFree((void*)input_gpu);
+        cudaFree((void*)output_gpu);
+        return input;
+    }
+
+    gelu_kernel <<<num_blocks, block_size>>> (input_gpu, output_gpu, size);
+
+    std::vector<float> output(size);
+    if(cudaMemcpy((void*)output.data(), (void*)output_gpu, bytes, cudaMemcpyDeviceToHost) != cudaSuccess)
+    {
+        cudaFree((void*)input_gpu);
+        cudaFree((void*)output_gpu);
+        return input;
+    }
+
+    cudaFree((void*)input_gpu);
+    cudaFree((void*)output_gpu);
+
+    return output;
+}
